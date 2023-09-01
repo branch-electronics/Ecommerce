efbd39e95d5703f8f257e71086c8836bcea0d2ef
ANJANEY SRINIVAS <82366870+branch-electronics@users.noreply.github.com>
2023-09-01 18:21:39 +0530

    Update CheckoutService.java

--- 02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/service/CheckoutService.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/service/CheckoutService.java
@@ -3,11 +3,11 @@ package com.example.ecommerce.service;
 import com.example.ecommerce.dto.PaymentInfo;
 import com.example.ecommerce.dto.Purchase;
 import com.example.ecommerce.dto.PurchaseResponse;
-import com.stripe.exception.StripeException;
-import com.stripe.model.PaymentIntent;
+//import com.stripe.exception.StripeException;
+//import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
-    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
+    //PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 
 }
